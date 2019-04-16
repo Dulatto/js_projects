5490b7963c78584031aa47b96870a0f157afa1c5
Duka <dulat.omarov@gmail.com>
2019-04-16 14:12:54 -0400

    es6 example

--- yoga2/js/script.js
+++ yoga2/js/script.js
@@ -103,3 +103,14 @@ window.addEventListener('DOMContentLoaded', function(){
      });
 
 });
+
+// Второе задание
+
+// let age = document.getElementById('age');
+ 
+// function showUser(surname, name) {
+//          alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// }
+ 
+// showUser.apply(age, ["Ivanov","Ivan"]);
+
